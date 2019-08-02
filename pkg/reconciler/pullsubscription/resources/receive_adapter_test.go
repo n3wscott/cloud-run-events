a2d753db57b02c011b380b255ea2c5f39fd0c912
Scott Nichols <nicholss@google.com>
2019-08-02 11:35:04 -0700

    missed one.

--- pkg/reconciler/pullsubscription/resources/receive_adapter_test.go
+++ pkg/reconciler/pullsubscription/resources/receive_adapter_test.go
@@ -19,8 +19,8 @@ package resources
 import (
 	"testing"
 
-	"github.com/googlecloudplatform/cloud-run-events/pkg/apis/pubsub/v1alpha1"
 	"github.com/google/go-cmp/cmp"
+	"github.com/googlecloudplatform/cloud-run-events/pkg/apis/pubsub/v1alpha1"
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
